10cb80463b1d183f601ea4cf214d303a8092d3c7
GitSzuKai <GitSzuKai@users.noreply.github.com>
2017-02-17 22:38:51 -0500

    Got all data in label and uiimage

--- ios_yelp_swift-master/Yelp/BusinessCellTableViewCell.swift
+++ ios_yelp_swift-master/Yelp/BusinessCellTableViewCell.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class BusinessCellTableViewCell: UITableViewCell {
-
+    
+    var business: Business!{
+        didSet {
+            titleLabel.text = business.name
+            foodImage.setImageWith(business.imageURL!)
+            category.text = business.categories
+            addressLabel.text = business.address
+            reviewLabel.text = "\(business.reviewCount!) Reviews"
+            starImage.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var moneyLabel: UILabel!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var starImage: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        foodImage.layer.cornerRadius = 5
+        foodImage.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- ios_yelp_swift-master/Yelp/BusinessesViewController.swift
+++ ios_yelp_swift-master/Yelp/BusinessesViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
     var businesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.tableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -56,4 +60,18 @@ class BusinessesViewController: UIViewController {
      }
      */
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil{
+            return businesses.count
+        }else{return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCellTableViewCell
+        cell.business = businesses[indexPath.row]
+        
+        return cell
+        
+            }
 }
